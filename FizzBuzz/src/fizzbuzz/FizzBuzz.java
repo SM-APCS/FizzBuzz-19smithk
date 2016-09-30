fa9b2552c6423f069b398d9c9bf09ec748f190f2
19smithk <19smithk@smtexas.org>
2016-09-30 13:58:13 -0500

    FIZZBUZZ!

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -7,7 +7,7 @@ package fizzbuzz;
 
 /**
  *
- * @author Mr. T - CHANGE TO YOUR NAME
+ * @author Kyle Smith
  * @class AP CSA
  */
 public class FizzBuzz {
@@ -16,7 +16,26 @@ public class FizzBuzz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //created "for" loop to create repetition
+        for (int i = 0; i <= 100; i++)
+        {
+            //see if variable i is divisible by 3
+            if (i % 3 == 0)
+            {
+                System.out.println("fizz");
+            //see if the variable i is divisible by 5 as well as by the previous 3
+                if (i % 5 == 0)
+                {
+                    System.out.println("FizzBuzz");
+                }
+            }
+            //sees whether the variable i is JUST divisible by 5
+            else if ( i % 5 == 0)
+            {
+                    System.out.println("buzz");
+            }
+        }
+
     }
     
 }
